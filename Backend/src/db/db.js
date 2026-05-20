6fe0a1fd88b64786514d4509dcdcdc8cbee3cb70
Amit-cl <amitkumar2182004@gmail.com>
2026-05-20 20:28:25 +0530

    final commit fixed frontend

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   await mongoose.connect("mongodb://127.0.0.1:27017/project-1");
-  console.log("DB connected");
+   console.log("DB connected");
 }
 
 module.exports = connectDB;
